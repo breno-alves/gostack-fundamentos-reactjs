14781424b283aa724440087d35f68b4de9cc7e72
Breno Alves <breno550@hotmail.com>
2020-12-28 13:47:41 -0300

    solucao

--- src/utils/formatValue.ts
+++ src/utils/formatValue.ts
@@ -1,4 +1,12 @@
-const formatValue = (value: number): string =>
-  Intl.NumberFormat().format(value); // TODO
+const formatValue = (value: number): string => {
+  return Intl.NumberFormat('pt-BR', {
+    style: 'currency',
+    currency: 'BRL',
+  }).format(value);
+  // return Number(value).toLocaleString('pt-BR', {
+  //   minimumFractionDigits: 2,
+  //   maximumFractionDigits: 2,
+  // });
+};
 
 export default formatValue;
